74faa5c155f1c499bfaa879ff16411629092ebc3
abdifatahmuse <cabdifataaxmuuse@gmail.com>
2025-02-27 10:33:49 +0300

    📦️ Components

---
+++ src/components/auth/auth-wrapper.tsx
@@ -0,0 +1,38 @@
+import { GalleryVerticalEnd } from "lucide-react";
+import ModeToggle from "../mode-toggle";
+
+export default function AuthWrapper({
+  children,
+  title,
+  image,
+}: {
+  children: React.ReactNode;
+  title: string;
+  image: string;
+}) {
+  return (
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex justify-center gap-2 md:justify-start">
+          <a href="#" className="flex items-center gap-2 font-medium">
+            <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
+              <GalleryVerticalEnd className="size-4" />
+            </div>
+            {title}
+          </a>
+          <ModeToggle />
+        </div>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">{children}</div>
+        </div>
+      </div>
+      <div className="relative hidden bg-muted lg:block">
+        <img
+          src={image}
+          alt="Image"
+          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.9] dark:grayscale"
+        />
+      </div>
+    </div>
+  );
+}
