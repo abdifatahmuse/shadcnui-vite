adf53ae0e2a11a2a5945ede0a0eb7e9b1424e93b
abdifatahmuse <cabdifataaxmuuse@gmail.com>
2025-02-27 10:20:12 +0300

    :lipstick: Setup Theme Mode

---
+++ src/components/mode-toggle.tsx
@@ -0,0 +1,80 @@
+import { Moon, MoonIcon, Sun, SunIcon } from "lucide-react";
+
+import { useTheme } from "@/components/theme-provider";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { cn } from "@/lib/utils";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@radix-ui/react-tooltip";
+import React from "react";
+
+export default function ModeToggle({
+  showLabel = false,
+  iconOnly = false,
+}: {
+  showLabel?: boolean;
+  iconOnly?: boolean;
+}) {
+  const { setTheme, theme } = useTheme();
+
+  const toggleTheme = React.useCallback(() => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  }, [theme, setTheme]);
+
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          {iconOnly ? (
+            <Button
+              variant="ghost"
+              className={cn(
+                "group/toggle h-8 w-8 px-0",
+                showLabel && "px-5 w-16"
+              )}
+              onClick={toggleTheme}
+            >
+              <SunIcon className="hidden [html.dark_&]:block" />
+              <MoonIcon className="hidden [html.light_&]:block" />
+              <span className="sr-only">Toggle theme</span>
+              {showLabel && <label htmlFor="theme">{theme}</label>}
+            </Button>
+          ) : (
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="outline" size="icon">
+                  <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                  <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                  <span className="sr-only">Toggle theme</span>
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuItem onClick={() => setTheme("light")}>
+                  Light
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setTheme("dark")}>
+                  Dark
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setTheme("system")}>
+                  System
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          )}
+        </TooltipTrigger>
+        <TooltipContent>
+          <label>Theme Mode</label>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,18 @@
-import { Theme } from "@radix-ui/themes";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
-import App from "./App.tsx";
-import "./styles.css";
+import { ThemeProvider } from "./components/theme-provider.tsx";
+import "./global.css";
+import { Routers } from "./routes/Routers.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <Theme
-        accentColor="blue"
-        // grayColor="sand"
-        grayColor="slate"
-        radius="large"
-        scaling="95%"
-        appearance="light"
-        panelBackground="translucent"
-      >
-        <App />
-        {/* <ThemePanel /> */}
-      </Theme>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        {/* <App /> */}
+        {/* <App /> */}
+        <Routers />
+      </ThemeProvider>
     </BrowserRouter>
   </StrictMode>
 );
